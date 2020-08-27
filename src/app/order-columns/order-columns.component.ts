29ed12a9beaad2a2af67983a037081dd47786c7b
Patrick_Desktop <paddymcpherson@hotmail.com>
2020-08-27 16:26:34 +0100

    async idea hasn't worked yet

--- src/app/order-columns/order-columns.component.ts
+++ src/app/order-columns/order-columns.component.ts
@@ -68,54 +68,63 @@ export class OrderColumnsComponent implements OnInit {
   }
 
   sortOrders() {
-    for (let i in this.ordersJSON) {
-      if (this.ordersJSON[i].State === 'ordered') {
-        this.ordered.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'instock') {
-        this.instock.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'soaked') {
-        this.soaked.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'tied') {
-        this.tied.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'dyed') {
-        this.dyed.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'rinsed') {
-        this.rinsed.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'washed') {
-        this.washed.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'dried') {
-        this.dried.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'ironed') {
-        this.ironed.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'packaged') {
-        this.packaged.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'posted') {
-        this.posted.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'delivered') {
-        this.delivered.push(this.ordersJSON[i]);
-      } else if (this.ordersJSON[i].State === 'completed') {
-        this.completed.push(this.ordersJSON[i]);
-      }
-    }
+
   }
 
-  getOrders(): void {
+  getOrders() {
     this.orderService.getOrders()
       .subscribe((orders: Order[]) => {
         this.ordersJSON = orders;
-        console.log(this.ordersJSON);
-        console.log(this.completed);
+        for (let i in this.ordersJSON) {
+          if (this.ordersJSON[i].State === 'ordered') {
+            this.ordered.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'instock') {
+            this.instock.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'soaked') {
+            this.soaked.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'tied') {
+            this.tied.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'dyed') {
+            this.dyed.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'rinsed') {
+            this.rinsed.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'washed') {
+            this.washed.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'dried') {
+            this.dried.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'ironed') {
+            this.ironed.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'packaged') {
+            this.packaged.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'posted') {
+            this.posted.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'delivered') {
+            this.delivered.push(this.ordersJSON[i]);
+          } else if (this.ordersJSON[i].State === 'completed') {
+            this.completed.push(this.ordersJSON[i]);
+          }
+        }
       });
   }
   // can do the assigning of orders here instead of
   // making an all orders array
 
 
+  async fetchAndSort() {
+    await this.getOrders();
+    console.log(this.ordersJSON);
+    await this.sortOrders();
+    console.log(this.ordersJSON);
+    console.log(this.completed);
+  }
+
   constructor(private orderService: OrderService) {
   }
 
   ngOnInit() {
     this.getOrders();
+    console.log(this.ordersJSON)
+
   }
 }
 
